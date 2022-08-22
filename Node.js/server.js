8858ddf14971839c95bf41006cb58a576fd1e01b
전채원 <chaewon6400@gmail.com>
2022-08-22 20:07:53 +0900

    form post전송

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(8080, function () {
   console.log("listening on 8080");
@@ -17,4 +19,15 @@ app.get("/beauty", function (req, res) {
 // /하나만 쓰면 home을 의미
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/write", function (req, res) {
+  res.sendFile(__dirname + "/write.html");
+});
+
+// 폼 post 전송 -> 처리
+app.post("/add", function (req, res) {
+  res.send("폼 전송 완료");
+  console.log(req.body.title);
+  console.log(req.body.date);
 });
