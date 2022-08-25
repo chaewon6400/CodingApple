7cab8e950b07fb352fe7f77bd8293886ec79992f
전채원 <chaewon6400@gmail.com>
2022-08-26 01:20:08 +0900

    이벤트버블링 마무리

--- JavaScript JQuery/tab.js
+++ JavaScript JQuery/tab.js
@@ -37,13 +37,22 @@
 // }
 
 // 더 더 확장성 있는 코드
-for (let i = 0; i < $(".tab-button").length; i++) {
-  $(".tab-button")
-    .eq(i)
-    .on("click", function () {
-      $(".tab-button").removeClass("orange");
-      $(".tab-button").eq(i).addClass("orange");
-      $(".tab-content").removeClass("show");
-      $(".tab-content").eq(i).addClass("show");
-    });
+// for (let i = 0; i < $(".tab-button").length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//       openTab(i);
+//     });
+// }
+
+function openTab(i) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(i).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(i).addClass("show");
 }
+
+// 탭기능 다르게 만들어보기 (이벤트리스너 1개만 써서!)
+$(".list").click(function (e) {
+  openTab(e.target.dataset.id);
+});
